cc65dea2dc411bb0efbab16ce8015f72f5bf4313
Roberto-Hoo <prof.robertohoo@hotmail.com>
2021-09-06 13:22:14 -0300

    ok

--- aula_3_exercicio.py
+++ aula_3_exercicio.py
@@ -1,19 +1,44 @@
-import modulo as mod_1
+import modulo as m1
 
-mod_1.fib(515000)
+m1.fib(515000)
 print("")
-mod_1.fib2(30)
+m1.fib2(30)
 k = 1 or False
 print('')
 print(k)
 
+m1.nl()
 for n in range(2, 20):
-        resultado=mod_1.eh_primo(n)
-        if resultado==True:
-            print(n, 'é um número primo')
-        else:
-            print(n, 'igual', resultado, '*', n // resultado)
+    resultado = m1.eh_primo(n)
+    if resultado == True:
+        print(n, 'é um número primo')
+    else:
+        print(n, 'igual', resultado, '*', n // resultado)
 
-print('\n***************************************************************************\n')
+m1.nl()
+m1.lista_numeros_primos(20)
 
-mod_1.lista_numeros_primos(20)
+m1.nl()
+m1.lista_numeros_primos2(20)
+
+m1.nl()
+for c in range(-100, 201, 20):
+    print(f'{c:>7.2f} Celsius = {m1.celsius_to_fahrenheit(c):>7.2f} Fahrenheit')
+
+m1.nl()
+for f in range(-200, 401, 20):
+    print(f'{f:>7.2f} Fahrenheit = {m1.fahrenheit_to_celsius(f):>7.2f} Celsius')
+
+m1.nl()
+for c in range(-100, 201, 20):
+    print(f'{c:>7.2f} Celsius = {m1.fahrenheit_to_celsius(m1.celsius_to_fahrenheit(c)):>7.2f} Celsius')
+
+m1.nl()
+a, b, c = 1, -1, -2
+print(f'As raízes de {a:>5.1f}x**2 + {b:>5.1f}x + {c:>5.1f} são:\n')
+m1.Baskara(a,b,c)
+
+m1.nl()
+a, b, c = 1, -1, 2.5
+print(f'As raízes de {a:>5.1f}x**2 + {b:>5.1f}x + {c:>5.1f} são:\n')
+m1.Baskara(a,b,c)
--- modulo.py
+++ modulo.py
@@ -1,4 +1,4 @@
-import math
+import math,cmath
 
 
 def fib(n):
@@ -9,7 +9,7 @@ def fib(n):
     while i < n:
         print(i, end=' ')  # O argumento end pode ser usado para evitar uma nova linha após a saída
         i, j = j, i + j  # Atualiza pelos valores antigos i<--j(antigo) e j<--i(antigo)+j(antigo)
-    return
+    return None
 
 
 def fib_lista(n):
@@ -41,14 +41,18 @@ def fib2(m):
         a2 = a2 + a1
         a1 = aux
         n = n + 1
-    return
+    return None
 
 
 def eh_primo(n):
     """
     Calcula se n é numero primo ou não
-    Entrada;
+    Entrada:
         int n: número Natural maior que 1
+    Saída:
+        Retorma True se n é primo, em caso contrário retorna o menor divisor
+        de n, maior ou igual a 2, isto é o menor número primo que pode
+        dividir n
     """
     if n == 2:
         return True
@@ -63,8 +67,10 @@ def eh_primo(n):
 def lista_numeros_primos(m):
     """
     Calcula os números primos de 2 a m
-    Entrada;
-        int n: número Natural maior que 1
+    Entrada:
+        int m: número Natural maior que 2
+    Saída;
+        None.  Mas imprime todos os numéros primos e não primos de 2 até m.
     """
     for n in range(2, m):
         resultado = eh_primo(n)
@@ -74,3 +80,64 @@ def lista_numeros_primos(m):
             print(n, 'igual', resultado, '*', n // resultado)
 
     return None
+
+def lista_numeros_primos2(m):
+    """
+    Calcula os primeiro m números primos
+    Entrada:
+        int m: número Natural maior que 0
+    Saída;
+        None. Mas imprime todos os numéros primos e não primos até o m-ésimo número primo.
+        (Do número 2 em diante)
+    """
+    n=0
+    k=2
+    while n < m: # n=1,2,3,..,m
+        resultado = eh_primo(k)
+        if resultado == True:
+            print(k, 'é um número primo')
+            n=n+1
+        else:
+            print(k, 'igual', resultado, '*', k // resultado)
+        k=k+1
+    return None
+
+def celsius_to_fahrenheit(c):
+    """
+    Converte Celsius para Fahrenheit
+    Entrada:
+        float c: temperatura em Celsius
+    Saída;
+        float f: temperatura em Fahrenheit.
+    """
+    return (c*9/5)+32
+
+def nl():
+    """Imprime nova linha com asteristicos *********************************************"""
+    print('\n***************************************************************************\n')
+    return None
+
+def fahrenheit_to_celsius(f):
+    """
+    Converte Fahrenheit para Celsius
+    Entrada:
+        float f: temperatura em Fahrenheit
+    Saída;
+        float c: temperatura em Celsius.
+    """
+    return (f-32)*5/9
+
+def Baskara(a,b,c):
+    delta = b*b-4*a*c
+    if delta < 0:
+        raiz=math.sqrt(-delta)
+        z1=complex(-b/(2*a), raiz/(2*a))
+        z2=complex(-b/(2*a), -raiz/(2*a))
+        print('A raíses complexas são: ',z1,' e ', z2)
+        return [-1,z1,z2]
+    else:
+        raiz = math.sqrt(delta)
+        x1 = (-b + raiz) / (2 * a)
+        x2 = (-b - raiz) / (2 * a)
+        print('A raíses reais são:', x1, ' e ', x2)
+        return [+1,x1, x2]
